3837b23b076598ed1bf630c56a5d3e5099dd29d7
Paul <isakura313@gmail.com>
2020-08-27 14:04:37 +0300

    asdas

--- code.js
+++ code.js
@@ -21,3 +21,26 @@ cross.onclick = () =>{
         wrapper_form.classList.remove("anim_gone");
     }, 1000);
 }
+//у нас есть faq  quest на них клик, разворачивается ответ
+// желательно плавные анимации
+// и нужно создать стрелочку, которая смотрит вниз
+// при закрытом вопроса, и поворачивается
+let faq_quest = document.querySelectorAll(".faq_quest");
+let faq_answer = document.querySelectorAll(".faq_answer");
+
+for (let i = 0; i < faq_quest.length; i++) {
+    let icon = document.createElement("i");
+    icon.className = "fa fa-arrow-up";
+    faq_quest[i].append(icon);
+}
+icons = document.querySelectorAll(".fa-arrow-up");
+
+for (let i = 0; i < faq_quest.length; i++) {
+    faq_quest[i].onclick = () =>{
+        faq_answer[i].classList.toggle("visible_helper");
+        icons[i].classList.toggle("transform_helper");
+}
+}
+
+
+
